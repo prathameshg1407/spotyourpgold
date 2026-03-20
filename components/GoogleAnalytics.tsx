285650fc83aa30227c4941c5fe3c6294d1078bb0
SarveshBhoite <rajb81008@gmail.com>
2026-03-20 22:43:13 +0530

    scripts added

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -3,6 +3,6 @@
 import { GoogleAnalytics as NextGoogleAnalytics } from "@next/third-parties/google";
 
 export default function GoogleAnalytics() {
-  return <NextGoogleAnalytics gaId="G-5PWF1SMC4Q" />;
+  return <NextGoogleAnalytics gaId="G-48KNTNT35R" />;
 }
 
