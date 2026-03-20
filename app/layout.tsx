65e202f5ad138508591385c5afbf0c50148caf3e
SarveshBhoite <rajb81008@gmail.com>
2026-03-20 22:59:55 +0530

    clarity script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Poppins, Zen_Tokyo_Zoo, Parisienne } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { DockWrapper } from "@/components/DockWrapper";
 import { Toaster } from "@/components/ui/sonner";
@@ -55,6 +56,15 @@ export default function RootLayout({
       <body
         className={` ${inter.variable} ${poppins.variable} ${parisienne.variable} `}
       >
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vysj57fe51");
+          `}
+        </Script>
         <GoogleAnalytics />
         <AuthProvider />
         <PageLoader />
